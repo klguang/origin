f636db270f8da0e62abb5888b05ea283ad3ef596
Tom Linton <tom@originprotocol.com>
2019-03-13 08:46:20 +1300

    Cleanup

--- scripts/clean-package-locks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-find . -maxdepth 2 -name package-lock.json -type f -delete
--- scripts/symlink-packages.js
+++
@@ -1,7 +0,0 @@
-// Symlinks packages for origin-js that don't play nicely with lerna --hoist
-
-const { exec } = require('child_process')
-
-exec('ln -s ../../node_modules/scrypt origin-js/node_modules/scrypt')
-exec('ln -s ../../node_modules/got origin-js/node_modules/got')
-
